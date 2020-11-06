db0d55b3bc432d1a1df1003efc413811338bbe44
Jaden Lee <alwaysjaden@gmail.com>
2020-11-05 20:13:35 -0800

    all functions are working now

--- script.js
+++ script.js
@@ -54,14 +54,13 @@ function CurrentDate() {
             // if ID < timeValue = past
             // if ID = timeValue = current
             // if ID > timeValue = future 
-        if ( timeValue[i] === currentHour)
+        if ( timeValue[i] == currentHour)
         { inputSec.setAttribute("class","present col-lg-8 description textarea");
         } else if (timeValue[i] <= currentHour) {
             inputSec.setAttribute("class","past col-lg-8 description textarea");
         } else { 
             inputSec.setAttribute("class","future col-lg-8 description textarea");
         }
-
         //Create Save Button
         var inputEl = document.querySelectorAll("input");
         var saveBtn = document.createElement("button");
@@ -70,9 +69,6 @@ function CurrentDate() {
         saveBtn.setAttribute("onclick", "saveToLocal()");
         saveBtn.setAttribute("style", "id = Btn"+[i]);
         timeRow[i].appendChild(saveBtn);   
-
-        
-        
         };
 
         var savebtn = document.querySelectorAll("button");
@@ -81,25 +77,49 @@ function CurrentDate() {
         console.log(inputEl.value)
     
     SavedSchedule ()
-
+console.log(inputEl.length);
 
     // Save input Value to local storage funtion
     function saveToLocal(){
-       
+        // forloop here !! lets Try
         var scheduleList = {
-            schedule9am : inputEl[0].value // need to set Value from input Value
+            schedule9am: inputEl[0].value,
+            schedule10am: inputEl[1].value,
+            schedule11am: inputEl[2].value,
+            schedule12pm: inputEl[3].value, 
+            schedule1pm: inputEl[4].value, 
+            schedule2pm: inputEl[5].value, 
+            schedule3pm: inputEl[6].value, 
+            schedule4pm: inputEl[7].value,
+            schedule5pm: inputEl[8].value, 
+            schedule6pm: inputEl[9].value,  
+             // need to set Value from input Value
             // inputEl[0].value.trim()
         };
     localStorage.setItem("schedule", JSON.stringify(scheduleList));
+    
     };
 
     // display local storage value function
     function SavedSchedule () {
         var allSchedules = localStorage.getItem("schedule"); 
 
+        // if (allSchedules===null)
+        // display "" as empty value
+
         console.log(allSchedules)
         allSchedules = JSON.parse(allSchedules);
         inputEl[0].setAttribute("value", allSchedules.schedule9am);
+        inputEl[1].setAttribute("value", allSchedules.schedule10am);
+        inputEl[2].setAttribute("value", allSchedules.schedule11am);
+        inputEl[3].setAttribute("value", allSchedules.schedule12pm);
+        inputEl[4].setAttribute("value", allSchedules.schedule1pm);
+        inputEl[5].setAttribute("value", allSchedules.schedule2pm);
+        inputEl[6].setAttribute("value", allSchedules.schedule3pm);
+        inputEl[7].setAttribute("value", allSchedules.schedule4pm);
+        inputEl[8].setAttribute("value", allSchedules.schedule5pm);
+        inputEl[9].setAttribute("value", allSchedules.schedule6pm);
+
     }
 
 
