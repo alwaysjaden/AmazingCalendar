842b5830426b9758841b3b71b5536b3f089572d4
Jaden Lee <alwaysjaden@gmail.com>
2020-11-07 09:52:03 -0800

    fixed local saving issue

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function CurrentDate() {
        
         //  Create Schedule input Section  
         var inputSec = $("<input class ='col-lg-8 description textArea' type ='text' placeholder = 'No Schedule'>")
-        inputSec.attr("id",timeValue[i])
+        inputSec.attr("id","schedule"+[i])
         
         // Change calss attribute of input box based on Current Time [ past ] [ present ] [ future ]
             // if ID < timeValue = past if ID = timeValue = current if ID > timeValue = future 
@@ -89,18 +89,28 @@ function CurrentDate() {
         // for (var i = 0 ; i<allSchedules.length; i++){
         // var allSchedules = localStorage.getItem("schedule"); 
        // get schedule from Local Storeage to display saved schedule
-
-        allSchedules = JSON.parse(allSchedules);
-        inputEl[0].setAttribute("value", allSchedules.schedule9am);
-        inputEl[1].setAttribute("value", allSchedules.schedule10am);
-        inputEl[2].setAttribute("value", allSchedules.schedule11am);
-        inputEl[3].setAttribute("value", allSchedules.schedule12pm);
-        inputEl[4].setAttribute("value", allSchedules.schedule1pm);
-        inputEl[5].setAttribute("value", allSchedules.schedule2pm);
-        inputEl[6].setAttribute("value", allSchedules.schedule3pm);
-        inputEl[7].setAttribute("value", allSchedules.schedule4pm);
-        inputEl[8].setAttribute("value", allSchedules.schedule5pm);
-        inputEl[9].setAttribute("value", allSchedules.schedule6pm);
+       allSchedules = JSON.parse(allSchedules);
+       $("#schedule0").attr("value", allSchedules.schedule9am);
+       $("#schedule1").attr("value", allSchedules.schedule10am);
+       $("#schedule2").attr("value", allSchedules.schedule11am);
+       $("#schedule3").attr("value", allSchedules.schedule12pm);
+       $("#schedule4").attr("value", allSchedules.schedule1pm);
+       $("#schedule5").attr("value", allSchedules.schedule2pm);
+       $("#schedule6").attr("value", allSchedules.schedule3pm);
+       $("#schedule7").attr("value", allSchedules.schedule4pm);
+       $("#schedule8").attr("value", allSchedules.schedule5pm);
+       $("#schedule9").attr("value", allSchedules.schedule6pm);
+        // allSchedules = JSON.parse(allSchedules);
+        // inputEl[0].setAttribute("value", allSchedules.schedule9am);
+        // inputEl[1].setAttribute("value", allSchedules.schedule10am);
+        // inputEl[2].setAttribute("value", allSchedules.schedule11am);
+        // inputEl[3].setAttribute("value", allSchedules.schedule12pm);
+        // inputEl[4].setAttribute("value", allSchedules.schedule1pm);
+        // inputEl[5].setAttribute("value", allSchedules.schedule2pm);
+        // inputEl[6].setAttribute("value", allSchedules.schedule3pm);
+        // inputEl[7].setAttribute("value", allSchedules.schedule4pm);
+        // inputEl[8].setAttribute("value", allSchedules.schedule5pm);
+        // inputEl[9].setAttribute("value", allSchedules.schedule6pm);
      
 
     // }
